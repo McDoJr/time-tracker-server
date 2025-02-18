c5fa206c2234140937f72cbabdfa3211c479c2a7
McDoJr <user@MacBookJr.local>
2025-02-18 15:10:38 +0800

    added realtime channel listener for database update, insert, and delete

--- server.js
+++ server.js
@@ -149,13 +149,13 @@ app.post("/api/delete", async (req, res) => {
 
 // WebSocket connection
 io.on("connection", (socket) => {
-    console.log("Client connected:", socket.id);
+    // console.log("Client connected:", socket.id);
 
     socket.on("disconnect", () => {
-        console.log("Client disconnected:", socket.id);
+        // console.log("Client disconnected:", socket.id);
     });
 });
 
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => console.log("Server running on port 3000"));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
